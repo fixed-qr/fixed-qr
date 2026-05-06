e2b0132835f8cfe235cda6068a151494a07f34e9
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-05-06 21:07:47 +0530

    feat: switch crypo to nanoid

--- src/utils/generate-transaction-id.ts
+++ src/utils/generate-transaction-id.ts
@@ -1,9 +1,7 @@
+import { nanoid } from "nanoid/non-secure";
+
 export function generateTransactionId(prefix = "TXN") {
-  const idPart = crypto
-    .randomUUID()
-    .replaceAll("-", "")
-    .slice(0, 6)
-    .toUpperCase();
+  const idPart = nanoid(6).toUpperCase();
 
   return `${prefix}-${Date.now()}-${idPart}`;
 }
