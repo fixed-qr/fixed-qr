ed0d45675ca54d8143974311b7529e1067b42b06
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-05-08 18:57:31 +0530

    feat: add amount component

---
+++ src/components/amount.tsx
@@ -0,0 +1,59 @@
+import { ThemeColor } from "@/constants/theme";
+import { useTheme } from "@/hooks/use-theme";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Image, ThemedText, ThemedView } from "./ui";
+
+const initialSize = 15.5;
+
+interface AmountProps {
+  amount: string;
+  size?: number;
+  color?: ThemeColor;
+}
+
+export function Amount({ amount, size, color }: AmountProps) {
+  const theme = useTheme();
+
+  return (
+    <ThemedView style={styles.amount}>
+      <Image
+        source={require("@/assets/images/icons/rupee-64.png")}
+        style={[
+          styles.unitImage,
+          {
+            tintColor: color || theme.text,
+            width: size || initialSize,
+            height: size || initialSize,
+          },
+        ]}
+      />
+      <ThemedText
+        style={[
+          styles.amountText,
+          {
+            color,
+            fontSize: size || initialSize + 6,
+          },
+        ]}
+      >
+        {amount}
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  amount: {
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  unitImage: {
+    height: initialSize,
+    width: initialSize,
+  },
+  amountText: {
+    fontSize: initialSize + 6,
+  },
+});
