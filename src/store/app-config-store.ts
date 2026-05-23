d718e8a1cce19d49705d57c1964f345b46b4a150
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-05-23 21:40:42 +0530

    feat: update app config store with cache

--- src/store/app-config-store.ts
+++ src/store/app-config-store.ts
@@ -1,17 +1,49 @@
+import { storage } from "@/storage/mmkv";
 import { AppConfig } from "@/types/app-config";
 import { create } from "zustand";
 
+const APP_CONFIG_KEY = "app-config";
+
+const defaultConfig: AppConfig = {
+  status: "online",
+  release: {
+    version: "1.0.0",
+    versionCode: 100,
+    title: "FixedQR Initial Release",
+    downloadUrl: "",
+    publishedAt: "2026-05-20T03:20:56Z",
+    notes: ["Initial release", "Welcome to FixedQR!"],
+  },
+};
+
 interface AppConfigState {
-  appConfig: AppConfig | null;
+  appConfig: AppConfig;
   loading: boolean;
   error: string | null;
+
   fetchAppConfig: () => Promise<void>;
 }
 
+const getCachedConfig = (): AppConfig => {
+  try {
+    const cached = storage.getString(APP_CONFIG_KEY);
+
+    if (!cached) {
+      storage.set(APP_CONFIG_KEY, JSON.stringify(defaultConfig));
+      return defaultConfig;
+    }
+
+    return JSON.parse(cached) as AppConfig;
+  } catch {
+    return defaultConfig;
+  }
+};
+
 export const useAppConfigStore = create<AppConfigState>((set) => ({
-  appConfig: null,
+  appConfig: getCachedConfig(),
   loading: false,
   error: null,
+
   fetchAppConfig: async () => {
     try {
       set({
@@ -20,7 +52,7 @@ export const useAppConfigStore = create<AppConfigState>((set) => ({
       });
 
       const response = await fetch(
-        `https://raw.githubusercontent.com/fixed-qr/fixed-qr/refs/heads/main/app-config.json`,
+        "https://raw.githubusercontent.com/fixed-qr/fixed-qr/refs/heads/main/app-config.json",
       );
 
       if (!response.ok) {
@@ -29,6 +61,9 @@ export const useAppConfigStore = create<AppConfigState>((set) => ({
 
       const data = (await response.json()) as AppConfig;
 
+      // persist cache
+      storage.set(APP_CONFIG_KEY, JSON.stringify(data));
+
       set({
         appConfig: data,
         loading: false,
@@ -36,6 +71,7 @@ export const useAppConfigStore = create<AppConfigState>((set) => ({
     } catch (err) {
       set({
         error: err instanceof Error ? err.message : "Failed to fetch data",
+
         loading: false,
       });
     }
