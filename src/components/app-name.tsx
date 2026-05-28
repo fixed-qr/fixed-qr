74941690977b4ad0bc692fdd4e16f81e58112c27
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-05-28 16:26:05 +0530

    feat: add app name component

---
+++ src/components/app-name.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { AppText } from "./app-ui";
+
+export function AppName() {
+  return (
+    <AppText variant="headingSmall" weight="600" color="primary">
+      FixedQR
+    </AppText>
+  );
+}
+
+const styles = StyleSheet.create({});
