d0fd3a0a598a4ab3a84ea61d35037f3481bb7b91
Shailesh <shaileshpandit141@gmail.com>
2026-06-02 20:36:32 +0530

    feat: add zustand upi app store

---
+++ src/store/upi-app-store.ts
@@ -0,0 +1,27 @@
+import { UpiApp } from "@/types/upi-app";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { createPersistOptions } from "./zustand/persist";
+
+interface UpiAppStore {
+  upiApp: UpiApp[];
+  addUpiApp: (upiApp: UpiApp) => void;
+  removeUpiApp: (upiId: string) => void;
+  clearUpiApp: () => void;
+}
+
+export const useUpiAppStore = create<UpiAppStore>()(
+  persist(
+    (set) => ({
+      upiApp: [],
+      addUpiApp: (upiApp) =>
+        set((state) => ({ upiApp: [upiApp, ...state.upiApp] })),
+      removeUpiApp: (upiId) =>
+        set((state) => ({
+          upiApp: state.upiApp.filter((upiApp) => upiApp.upiId !== upiId),
+        })),
+      clearUpiApp: () => set({ upiApp: [] }),
+    }),
+    createPersistOptions<UpiAppStore>("upi-app-store"),
+  ),
+);
