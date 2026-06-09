8c6b487dd1419a7153ffc82ef31e60a75b14ffd9
Shailesh <shaileshpandit141@gmail.com>
2026-06-09 22:27:26 +0530

    feat: update with custom alert modal

---
+++ src/components/saved-upi-app-card.tsx
@@ -0,0 +1,92 @@
+import { AlertModal } from "@/components";
+import { AppIcon, AppImage, AppText, AppView } from "@/components/app-ui";
+import { upiAppLogo } from "@/constants/upi-app-logo";
+import { useTheme } from "@/hooks/use-theme";
+import { useSavedUpiAppStore } from "@/store/saved-upi-app-store";
+import { UpiApp } from "@/types/upi-app";
+import { useState } from "react";
+import { Pressable, StyleSheet } from "react-native";
+
+interface SavedUpiAppCardProps {
+  upiApp: UpiApp;
+  isLast: boolean;
+}
+
+export function SavedUpiAppCard({
+  upiApp,
+  isLast,
+}: Readonly<SavedUpiAppCardProps>) {
+  const theme = useTheme();
+  const removeUpiApp = useSavedUpiAppStore((state) => state.removeUpiApp);
+  const [showAlert, setShowAlert] = useState(false);
+
+  return (
+    <>
+      <AppView
+        style={[
+          styles.upiId,
+          {
+            borderColor: theme.border.primary,
+            borderBottomWidth: isLast ? 0 : 1,
+          },
+        ]}
+      >
+        <AppView style={styles.left}>
+          <AppImage
+            source={upiAppLogo[upiApp.appName]}
+            style={styles.logoImage}
+          />
+          <AppView style={styles.upiIdInfo}>
+            <AppText variant="button">{upiApp.appName}</AppText>
+            <AppText variant="bodySmall" color="tertiary">
+              {upiApp.upiId}
+            </AppText>
+          </AppView>
+        </AppView>
+        <AppView style={styles.right}>
+          <Pressable
+            onPress={() => {
+              setShowAlert(true);
+            }}
+          >
+            <AppIcon name="close" size={18} color={theme.text.primary} />
+          </Pressable>
+        </AppView>
+      </AppView>
+
+      {/* Delete confirmation */}
+      <AlertModal
+        visible={showAlert}
+        title="Remove UPI ID"
+        message={`Are you sure you want to remove this UPI ID (${upiApp.appName})?`}
+        onCancel={() => setShowAlert(false)}
+        onConfirm={() => {
+          removeUpiApp(upiApp.appName);
+          setShowAlert(false);
+        }}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  upiId: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 12,
+    gap: 8,
+  },
+  left: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 8,
+  },
+  right: {},
+  logoImage: {
+    width: 24,
+    height: 24,
+  },
+  upiIdInfo: {
+    gap: 2,
+  },
+});
--- src/components/sections/saved-upi-app.tsx
+++ src/components/sections/saved-upi-app.tsx
@@ -1,46 +1,13 @@
 import { EmptyCard } from "@/components";
-import {
-  AppGroup,
-  AppIcon,
-  AppImage,
-  AppText,
-  AppView,
-} from "@/components/app-ui";
-import { upiAppLogo } from "@/constants/upi-app-logo";
-import { useTheme } from "@/hooks/use-theme";
+import { AppGroup, AppView } from "@/components/app-ui";
 import { useSavedUpiAppStore } from "@/store/saved-upi-app-store";
-import { UpiAppName } from "@/types/upi-app-name";
 import { useRouter } from "expo-router";
-import { Alert, Pressable, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { SavedUpiAppCard } from "../saved-upi-app-card";
 
 export function SavedUpiAppSection() {
-  const theme = useTheme();
   const router = useRouter();
   const savedUpiApps = useSavedUpiAppStore((state) => state.savedUpiApps);
-  const removeUpiApp = useSavedUpiAppStore((state) => state.removeUpiApp);
-
-  const handleRemoveUpiApp = (appName: UpiAppName) => {
-    Alert.alert(
-      "Remove UPI ID",
-      `Are you sure you want to remove this UPI ID (${appName})?`,
-      [
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
-        {
-          text: "Remove",
-          style: "destructive",
-          onPress: () => {
-            removeUpiApp(appName);
-          },
-        },
-      ],
-      {
-        cancelable: true,
-      },
-    );
-  };
 
   return (
     <AppView style={styles.container}>
@@ -53,39 +20,11 @@ export function SavedUpiAppSection() {
       >
         {Object.keys(savedUpiApps).length ? (
           Object.values(savedUpiApps).map((upiApp, index) => (
-            <AppView
-              key={upiApp.appName}
-              style={[
-                styles.upiId,
-                {
-                  borderColor: theme.border.primary,
-                  borderBottomWidth:
-                    Object.keys(savedUpiApps).length - 1 == index ? 0 : 1,
-                },
-              ]}
-            >
-              <AppView style={styles.left}>
-                <AppImage
-                  source={upiAppLogo[upiApp.appName]}
-                  style={styles.logoImage}
-                />
-                <AppView style={styles.upiIdInfo}>
-                  <AppText variant="button">{upiApp.appName}</AppText>
-                  <AppText variant="bodySmall" color="tertiary">
-                    {upiApp.upiId}
-                  </AppText>
-                </AppView>
-              </AppView>
-              <AppView style={styles.right}>
-                <Pressable
-                  onPress={() => {
-                    handleRemoveUpiApp(upiApp.appName);
-                  }}
-                >
-                  <AppIcon name="close" size={18} color={theme.text.primary} />
-                </Pressable>
-              </AppView>
-            </AppView>
+            <SavedUpiAppCard
+              key={upiApp.appName + index}
+              upiApp={upiApp}
+              isLast={Object.keys(savedUpiApps).length - 1 == index}
+            />
           ))
         ) : (
           <EmptyCard message="Your Saved UPI ID will appear here." />
