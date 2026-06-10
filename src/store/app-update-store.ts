0c27bb6a1429d34ede30808d4c07cb2277048b8e
Shailesh <shaileshpandit141@gmail.com>
2026-06-10 15:30:46 +0530

    feat: add app update store

---
+++ src/store/app-update-store.ts
@@ -0,0 +1,55 @@
+import { AppUpdate } from "@/types/app-update";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { createPersistOptions } from "./zustand/persist";
+
+interface AppUpdateStore {
+  appUpdate: AppUpdate | null;
+  isLoading: boolean;
+  error: string | null;
+
+  checkAppUpdate: () => Promise<void>;
+}
+
+export const useAppUpdateStore = create<AppUpdateStore>()(
+  persist(
+    (set) => ({
+      appUpdate: null,
+      isLoading: false,
+      error: null,
+
+      checkAppUpdate: async () => {
+        try {
+          set({
+            isLoading: true,
+            error: null,
+          });
+
+          const response = await fetch(
+            "https://raw.githubusercontent.com/fixed-qr/fixed-qr/refs/heads/main/app-update.json",
+          );
+
+          if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+          }
+
+          const appUpdate = (await response.json()) as AppUpdate;
+
+          set({
+            appUpdate,
+            isLoading: false,
+          });
+        } catch (error) {
+          set({
+            error:
+              error instanceof Error
+                ? error.message
+                : "Failed to check app update",
+            isLoading: false,
+          });
+        }
+      },
+    }),
+    createPersistOptions<AppUpdateStore>("app-update-store"),
+  ),
+);
---
+++ src/types/app-update.tsx
@@ -0,0 +1,9 @@
+export interface AppUpdate {
+  version: string;
+  versionCode: number;
+  title: string;
+  websiteUrl: string;
+  downloadUrl: string;
+  publishedAt: string;
+  notes: string[];
+}
