6fb72a642ecbb52d3986e3a518696dbfdb3aec80
Shailesh <shaileshpandit141@gmail.com>
2026-06-17 15:21:57 +0530

    feat: add app sheet store

--- src/components/app-ui/app-sheet.tsx
+++ src/components/app-ui/app-sheet.tsx
@@ -1,5 +1,6 @@
 import { borderRadius } from "@/constants/platform";
 import { useTheme } from "@/hooks/use-theme";
+import { useAppSheetStore } from "@/store/app-sheet-store";
 import {
   BottomSheetBackdrop,
   BottomSheetBackdropProps,
@@ -9,49 +10,34 @@ import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback, useEffect, useRef } from "react";
 import { BackHandler, StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useShallow } from "zustand/react/shallow";
 
 interface AppSheetProps {
   children: React.ReactNode;
-  enableDynamicSizing?: boolean;
-  snapPoints?: (string | number)[];
-  index?: number;
   containerStyle?: StyleProp<ViewStyle>;
 }
 
 export function AppSheet({
   children,
-  enableDynamicSizing,
-  snapPoints,
-  index,
   containerStyle,
 }: Readonly<AppSheetProps>) {
   const theme = useTheme();
   const insets = useSafeAreaInsets();
   const router = useRouter();
-
-  const sheetRef = useRef<BottomSheetModal>(null);
   const sheetOpen = useRef(false);
   const isAnimating = useRef(false);
 
-  const renderBackdrop = useCallback(
-    (props: BottomSheetBackdropProps) => (
-      <BottomSheetBackdrop
-        {...props}
-        appearsOnIndex={0}
-        disappearsOnIndex={-1}
-        opacity={0.25}
-      />
-    ),
-    [],
+  const { sheetRef, enableDynamicSizing, snapPoints } = useAppSheetStore(
+    useShallow((state) => ({
+      sheetRef: state.sheetRef,
+      enableDynamicSizing: state.enableDynamicSizing,
+      snapPoints: state.snapPoints,
+    })),
   );
 
   useEffect(() => {
     sheetRef.current?.present();
-  }, []);
-
-  const onDismiss = useCallback(() => {
-    router.back();
-  }, [router]);
+  }, [sheetRef]);
 
   useFocusEffect(
     useCallback(() => {
@@ -77,15 +63,21 @@ export function AppSheet({
       );
 
       return () => subscription.remove();
-    }, []),
+    }, [sheetRef]),
   );
 
   return (
     <BottomSheetModal
       ref={sheetRef}
-      index={index}
       snapPoints={snapPoints}
-      onDismiss={onDismiss}
+      enableDynamicSizing={enableDynamicSizing}
+      enablePanDownToClose={true}
+      topInset={insets.top}
+      bottomInset={insets.bottom}
+      backdropComponent={renderAppSheetBackdrop}
+      onDismiss={() => {
+        router.back();
+      }}
       onAnimate={() => {
         isAnimating.current = true;
       }}
@@ -93,11 +85,6 @@ export function AppSheet({
         sheetOpen.current = index !== -1;
         isAnimating.current = false;
       }}
-      enablePanDownToClose={true}
-      enableDynamicSizing={enableDynamicSizing}
-      topInset={insets.top}
-      bottomInset={insets.bottom}
-      backdropComponent={renderBackdrop}
       animationConfigs={{
         duration: 380,
       }}
@@ -116,5 +103,16 @@ export function AppSheet({
     >
       {children}
     </BottomSheetModal>
+  );
+}
+
+function renderAppSheetBackdrop(props: Readonly<BottomSheetBackdropProps>) {
+  return (
+    <BottomSheetBackdrop
+      {...props}
+      appearsOnIndex={0}
+      disappearsOnIndex={-1}
+      opacity={0.25}
+    />
   );
 }
