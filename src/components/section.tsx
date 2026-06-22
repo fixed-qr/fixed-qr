787507a639aacb3573f5863d4d384695efe78d62
Shailesh <shaileshpandit141@gmail.com>
2026-06-22 21:39:24 +0530

    refactor: change component name to section

---
+++ src/components/section.tsx
@@ -0,0 +1,63 @@
+import { useTheme } from "@/hooks/use-theme";
+import React, { ReactNode } from "react";
+import { Pressable, StyleSheet, ViewStyle } from "react-native";
+import { AppIcon, AppIconProps, AppText, AppView } from "./app-ui";
+
+export interface SectionProps {
+  title: string;
+  children: ReactNode;
+  onTitlePress?: () => void;
+  titleIconName?: AppIconProps["name"];
+  containerStyle?: ViewStyle;
+}
+
+export const Section = ({
+  title,
+  children,
+  onTitlePress,
+  titleIconName,
+  containerStyle,
+}: SectionProps) => {
+  const theme = useTheme();
+
+  return (
+    <AppView>
+      <Pressable style={styles.titleContainer} onPress={onTitlePress}>
+        <AppText variant="bodyMedium" color="tertiary" weight="600">
+          {title}
+        </AppText>
+        {titleIconName && (
+          <AppIcon name={titleIconName} size={18} color={theme.text.tertiary} />
+        )}
+      </Pressable>
+      <AppView
+        style={[
+          styles.sectionContainer,
+          {
+            backgroundColor: theme.background.secondary,
+            borderColor: theme.border.primary,
+          },
+          containerStyle,
+        ]}
+      >
+        {children}
+      </AppView>
+    </AppView>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingInline: 8,
+  },
+  sectionContainer: {
+    flex: 1,
+    marginTop: 8,
+    paddingHorizontal: 12,
+    borderRadius: 24,
+    borderWidth: 1,
+  },
+});
