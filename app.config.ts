cd6c3fad05d3f4ea497ba9f326832f9017a71ca5
Shailesh <shaileshpandit141@gmail.com>
2026-06-26 18:30:11 +0530

    chore: update app version

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import { ConfigContext, ExpoConfig } from "expo/config";
 
-const APP_VERSION = "1.0.3";
+const APP_VERSION = "1.1.0";
 
 const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_PREVIEW = process.env.APP_VARIANT === "preview";
